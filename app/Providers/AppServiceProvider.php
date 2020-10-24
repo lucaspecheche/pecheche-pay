cea2f40181d5b83e930bb8020b36438716f41687
Pecheche <lucaspecheche@gmail.com>
2020-10-24 01:24:05 -0300

    #transaction - Customer Identifier Rule created

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Rules\CustomerIdentifier;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -12,5 +13,14 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot()
     {
+        /** @var \Illuminate\Validation\Factory $validator */
+        $validator = $this->app->validator;
+
+        $validator->extend('is_customer', CustomerIdentifier::class);
+
+        $validator->replacer('is_customer', function($message, $attribute, $rule, $parameters) {
+            return str_replace(':models', implode(', ', $parameters), $message);
+        });
+
     }
 }
