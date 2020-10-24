12085defb6307631fe6ef48c81f057e14bd7dba3
Pecheche <lucaspecheche@gmail.com>
2020-10-24 17:01:28 -0300

    #main - Default return validation created

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request as HttpRequest;
+use Illuminate\Http\Response;
+use Illuminate\Validation\ValidationException;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
@@ -11,5 +14,29 @@ class Controller extends BaseController
         $callClass = static::class;
 
         return "$callClass@$action";
+    }
+
+    public function response($message = null, $artifacts = [], $status = 200)
+    {
+        return response()->json([
+            'message'   => $message ?? 'Success',
+            'artifacts' => $artifacts
+        ], $status);
+    }
+
+    protected function throwValidationException(HttpRequest $request, $validator): void
+    {
+        $error =  [
+            'shortMessage'       => 'invalidData',
+            'message'            => trans('validation.invalidData'),
+            'description'        => [],
+        ];
+
+        foreach ($validator->errors()->all() as $message) {
+            $error['description'][] = $message;
+        }
+
+        $response = response()->json($error, Response::HTTP_UNPROCESSABLE_ENTITY);
+        throw new ValidationException($validator, $response);
     }
 }
